ae71db85654daf397147a6968743263ccc6f43aa
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-31 19:11:21 +0300

    requested results

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 class ProdConfig(Config):
     '''
