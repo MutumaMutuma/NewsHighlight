0d78e020c0addf49392fcd3349e6a6489537e266
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-01 12:28:22 +0300

    added class articles to define articles

--- app/models/source.py
+++ app/models/source.py
@@ -7,4 +7,16 @@ class Source:
         self.id =id
         self.name = name
         self.description = description
-        
+
+class Articles:
+    '''
+    defines the articles objects
+    '''
+
+    def __init__(self, title, author, description, urlToImage, publishedAt, url):
+        self.title = title
+        self.author = author
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.url = url
