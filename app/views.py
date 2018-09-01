a71da010697b14760ecbb4ebd9b4905d2f140828
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-01 12:00:34 +0300

    added a new base url

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -9,3 +10,6 @@ def index():
     View root page function that returns the index page and its data
     '''
     
+    
+    title = 'Welcome to The best News Website ever'
+    return render_template('index.html', title = title)
