3c3f8ba949d2a88f1cef9e6ec2232db5e0413160
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-01 12:26:02 +0300

    added a articles base url

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 class ProdConfig(Config):
     '''
