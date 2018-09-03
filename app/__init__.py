bafa6d96a0046d127afc1e5d62fcf73f7ce28d0b
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-03 10:12:32 +0300

    creating a function that will take our configurations as an argument

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,29 @@
 from flask import Flask
-from .config import DevConfig
+from config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+     # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app   
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+
+# from app import views
